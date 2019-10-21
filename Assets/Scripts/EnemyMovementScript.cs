709a6ed3e271d253165650d207afd3596ed86ad1
Tristen Briggz <skaterbriggz@gmail.com>
2019-10-21 19:25:13 -0400

    Platforms and sky implemented

---
+++ Assets/Scripts/EnemyMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+
+    public float speed = 1f;
+    private bool canMove = true;
+    //public float bound_X = -11f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (canMove)
+        {
+
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp;
+
+            //if (temp.x < bound_X)
+            //    gameObject.SetActive(false);
+
+
+        }
+    }
+}
