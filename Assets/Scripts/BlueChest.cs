21dc8bc67fc2a868487a548d56029036068fc8a3
Tristen Briggz <skaterbriggz@gmail.com>
2019-10-31 10:35:17 -0400

    added final platform and enemies

---
+++ Assets/Scripts/BlueChest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueChest : MonoBehaviour
+{
+
+    private AudioSource explosionSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+
+        if (target.tag == "Player")
+        {
+
+            //Destroy(.gameObject);
+            gameObject.SetActive(false);
+            Score.scoreValue += 1000;
+            explosionSound.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+
+        if (target.tag == "Player")
+        {
+
+            //Destroy(.gameObject);
+            gameObject.SetActive(false);
+            Score.scoreValue += 100;
+            //explosionSound.Play();
+        }
+    }
+}
